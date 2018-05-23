0cae5899102e958eb4b4556d94bb6e335b805e80
EMEAAD\hnouts <hugo.nouts@epitech.eu>
2018-05-23 20:03:57 +0200

    loic saved the dy

---
+++ src/models/hangman.ts
@@ -0,0 +1,71 @@
+export class Hangman {
+    word: string;
+    array: any;
+    format: string;
+    guesses: any;
+    wrongGuessesCount: number;
+    status: string;
+
+    constructor() {
+        this.word = "bonjour";
+        this.array = [];
+        for (let i = 0; i < this.word.length; i++) {
+            this.array.push('_');
+        }
+        this.format = this.array.join(' ');
+        this.guesses = [];
+        this.wrongGuessesCount = 0;
+        this.status = "player";
+    }
+
+    wrongGuessCount(word: string, guesses: any) {
+        let failedGuess: any = [];
+        for (let i = 0; i < guesses.length; i++) {
+            let wrongGuess: any = guesses[i];
+            if (word.indexOf(guesses[i]) === -1) {
+                failedGuess.push(wrongGuess);
+            }
+        }
+        return failedGuess.length;
+    }
+
+    showGuess(word: string, guesses: any) {
+        let guessed: any = [];
+        let letters: any = word.split("");
+        for (let i = 0; i < letters.length; i++) {
+            if (guesses.indexOf(letters[i]) !== -1) {
+                guessed.push(letters[i]);
+            } else {
+                guessed.push("_");
+            }
+        }
+        return guessed.join(" ");
+    }
+
+    isWinner(word: string, guesses: any) {
+        let countWrong = this.wrongGuessCount(word, guesses);
+        let secretWord = word.split("").join(" ");
+        let correct = this.showGuess(word, guesses);
+        if (countWrong < 8 && secretWord === correct) {
+            return 'winner';
+        } else if (countWrong < 8) {
+            return 'player';
+        } else {
+            return 'loser';
+        }
+    }
+
+    async guessLetter(letter): Promise<object> {
+        let newGuesses = this.guesses.concat([letter]);
+        let newGuessCount = this.wrongGuessCount(this.word, newGuesses);
+        let showLetter = this.showGuess(this.word, newGuesses);
+
+        return {
+            word: this.word,
+            format: showLetter,
+            guesses: newGuesses,
+            wrongGuessCount: newGuessCount,
+            status: this.isWinner(this.word, newGuesses)
+        }
+    }
+}
